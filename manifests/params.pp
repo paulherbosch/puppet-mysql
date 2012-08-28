cd946af19c343c82457aff4af6a12d9c980417bf
fdammeke <fabian.dammekens@cegeka.be>
2012-08-28 13:53:52 +0200

    Initial params

---
+++ manifests/params.pp
@@ -0,0 +1,40 @@
+class mysql::params {
+
+  $mycnf = $operatingsystem ? {
+    /RedHat|Fedora|CentOS/ => "/etc/my.cnf",
+    default => "/etc/mysql/my.cnf",
+  }
+
+  $mycnfctx = "/files${mycnf}"
+
+  if ! $data_dir {
+    $real_data_dir = '/var/lib/mysql'
+  } else {
+    $real_data_dir = $data_dir
+  }
+
+  #$data_dir = $mysql_data_dir ? {
+  #  "" => "/var/lib/mysql",
+  #  default => $mysql_data_dir,
+  #}
+
+  $backup_dir = $mysql_backupdir ? {
+    "" => "/var/backups/mysql",
+    default => $mysql_backupdir,
+  }
+
+  $replication_binlog_format = $replication_binlog_format ? {
+    "" => "STATEMENT",
+    default => $replication_binlog_format,
+  }
+
+  $logfile_group = $mysql_logfile_group ? {
+    '' => $operatingsystem ? {
+        'RedHat' => 'mysql',
+        'Debian' => 'adm',
+        default => 'adm',
+      },
+    default => $mysql_logfile_group,
+  }
+
+}
