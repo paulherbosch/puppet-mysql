f7bd48a3746078f2adbe7d0efbc759a71343ce79
fdammeke <fabian.dammekens@cegeka.be>
2012-10-03 15:01:55 +0200

    Added local my.cnf

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,6 +11,7 @@ class mysql::params {
   }
 
   $mycnfctx = "/files${mycnf}"
+  $mylocalcnf = '/root/.my.cnf'
 
   if ! $mysql::server::data_dir {
     $real_data_dir = '/var/lib/mysql'
@@ -24,28 +25,9 @@ class mysql::params {
     $real_instance_type = $mysql::server::instance_type
   }
 
-  #$data_dir = $mysql_data_dir ? {
-  #  ""       => "/var/lib/mysql",
-  #  default  => $mysql_data_dir,
-  #}
-
-  #$backup_dir = $mysql_backupdir ? {
-  #  ''      => '/var/backups/mysql',
-  #  default => $mysql_backupdir,
-  #}
-
   #$replication_binlog_format = $replication_binlog_format ? {
   #  ''      => 'STATEMENT',
   #  default => $replication_binlog_format,
-  #}
-
-  #$logfile_group = $mysql_logfile_group ? {
-  #  ''       => $::operatingsystem ? {
-  #      'RedHat'  => 'mysql',
-  #      'Debian'  => 'adm',
-  #      default   => 'adm',
-  #    },
-  #  default  => $mysql_logfile_group,
   #}
 
 }
