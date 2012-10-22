16b7cbf189920691f4142f41f4a34dea90c0a18a
fdammeke <fabian.dammekens@cegeka.be>
2012-10-22 12:03:10 +0200

    Mysql exists fact check fixed

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,7 +1,9 @@
 define mysql::database($ensure) {
-  if $::mysql_exists == true {
+    if $::mysql_exists {
     mysql_database { $name:
       ensure => $ensure,
     }
-  }
+    } else {
+      fail("Mysql binary not found, Fact[::mysql_exists]:${::mysql_exists}")
+    }
 }
