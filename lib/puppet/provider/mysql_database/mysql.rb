987bc35341eefb37954cd30913ae336863e94fd3
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-02-18 16:31:41 +0100

    Modified mysql type and provider for database/grant/user due to constant applying of grants

--- lib/puppet/provider/mysql_database/mysql.rb
+++ lib/puppet/provider/mysql_database/mysql.rb
@@ -1,67 +1,33 @@
-require 'puppet/provider/package'
+Puppet::Type.type(:mysql_database).provide(:mysql) do
 
-Puppet::Type.type(:mysql_database).provide(:mysql,
-		:parent => Puppet::Provider::Package) do
+  desc "Manages MySQL database."
 
-	desc "Use mysql as database."
-	commands :mysqladmin => '/usr/bin/mysqladmin'
-	commands :mysql => '/usr/bin/mysql'
+  defaultfor :kernel => 'Linux'
 
-	# retrieve the current set of mysql users
-	def self.instances
-		dbs = []
+  optional_commands :mysql      => 'mysql'
+  optional_commands :mysqladmin => 'mysqladmin'
 
-		cmd = "#{command(:mysql)} #{defaults_file} mysql -NBe 'show databases'"
-		execpipe(cmd) do |process|
-			process.each do |line|
-				dbs << new( { :ensure => :present, :name => line.chomp } )
-			end
-		end
-		return dbs
-	end
-
-	def query
-		result = {
-			:name => @resource[:name],
-			:ensure => :absent
-		}
-
-		cmd = "#{command(:mysql)} #{defaults_file} mysql -NBe 'show databases'"
-		execpipe(cmd) do |process|
-			process.each do |line|
-				if line.chomp.eql?(@resource[:name])
-					result[:ensure] = :present
-				end
-			end
-		end
-		result
-	end
-
-	def create
-		mysqladmin defaults_file, "create", @resource[:name]
-	end
-	def destroy
-		mysqladmin defaults_file, "-f", "drop", @resource[:name]
-	end
-
-	def exists?
-		if mysql([defaults_file, "-NBe", "show databases"].compact).match(/^#{@resource[:name]}$/)
-			true
-		else
-			false
-		end
-	end
-
-  # Optional defaults file
-  def self.defaults_file
-    if File.file?("#{Facter.value(:root_home)}/.my.cnf")
-      "--defaults-file=#{Facter.value(:root_home)}/.my.cnf"
-    else
-      nil
+  def self.instances
+    mysql('-NBe', "show databases").split("\n").collect do |name|
+      new(:name => name)
     end
   end
-  def defaults_file
-    self.class.defaults_file
-  end
-end
 
+  def create
+    mysql('-NBe', "create database `#{@resource[:name]}`")
+  end
+
+  def destroy
+    mysqladmin('-f', 'drop', @resource[:name])
+  end
+
+  def exists?
+    begin
+      mysql('-NBe', "show databases").match(/^#{@resource[:name]}$/)
+    rescue => e
+      debug(e.message)
+      return nil
+    end
+  end
+
+end
--- lib/puppet/provider/mysql_user/mysql.rb
+++ lib/puppet/provider/mysql_user/mysql.rb
@@ -1,88 +1,42 @@
-require 'puppet/provider/package'
+Puppet::Type.type(:mysql_user).provide(:mysql) do
 
-Puppet::Type.type(:mysql_user).provide(:mysql,
-		# T'is funny business, this code is quite generic
-		:parent => Puppet::Provider::Package) do
+  desc "manage users for a mysql database."
 
-	desc "Use mysql as database."
-	commands :mysql => '/usr/bin/mysql'
-	commands :mysqladmin => '/usr/bin/mysqladmin'
+  defaultfor :kernel => 'Linux'
 
-  # Optional defaults file
-  def self.defaults_file
-    if File.file?("#{Facter.value(:root_home)}/.my.cnf")
-      "--defaults-file=#{Facter.value(:root_home)}/.my.cnf"
-    else
-      nil
+  optional_commands :mysql      => 'mysql'
+  optional_commands :mysqladmin => 'mysqladmin'
+
+  def self.instances
+    users = mysql("mysql", '-BNe' "select concat(User, '@',Host) as User from mysql.user").split("\n")
+    users.select{ |user| user =~ /.+@/ }.collect do |name|
+      new(:name => name)
     end
   end
-  def defaults_file
-    self.class.defaults_file
+
+  def create
+    mysql("mysql", "-e", "create user '%s' identified by PASSWORD '%s'" % [ @resource[:name].sub("@", "'@'"), @resource.value(:password_hash) ])
   end
 
-	# retrieve the current set of mysql users
-	def self.instances
-		users = []
+  def destroy
+    mysql("mysql", "-e", "drop user '%s'" % @resource.value(:name).sub("@", "'@'") )
+  end
 
-		cmd = "#{command(:mysql)} #{defaults_file} mysql -NBe 'select concat(user, \"@\", host), password from user'"
-		execpipe(cmd) do |process|
-			process.each do |line|
-				users << new( query_line_to_hash(line) )
-			end
-		end
-		return users
-	end
+  def password_hash
+    mysql("mysql", "-NBe", "select password from user where CONCAT(user, '@', host) = '%s'" % @resource.value(:name)).chomp
+  end
 
-	def self.query_line_to_hash(line)
-		fields = line.chomp.split(/\t/)
-		{
-			:name => fields[0],
-			:password_hash => fields[1],
-			:ensure => :present
-		}
-	end
+  def password_hash=(string)
+    mysql("mysql", "-e", "SET PASSWORD FOR '%s' = '%s'" % [ @resource[:name].sub("@", "'@'"), string ] )
+  end
 
-	def mysql_flush 
-		mysqladmin defaults_file, "flush-privileges"
-	end
+  def exists?
+    not mysql("mysql", "-NBe", "select '1' from user where CONCAT(user, '@', host) = '%s'" % @resource.value(:name)).empty?
+  end
 
-	def query
-		result = {}
+  def flush
+    @property_hash.clear
+    mysqladmin "flush-privileges"
+  end
 
-		cmd = "#{command(:mysql)} #{defaults_file} -NBe 'select concat(user, \"@\", host), password from user where concat(user, \"@\", host) = \"%s\"'" % @resource[:name]
-		execpipe(cmd) do |process|
-			process.each do |line|
-				unless result.empty?
-					raise Puppet::Error,
-						"Got multiple results for user '%s'" % @resource[:name]
-				end
-				result = query_line_to_hash(line)
-			end
-		end
-		result
-	end
-
-	def create
-		mysql defaults_file, "mysql", "-e", "create user '%s' identified by PASSWORD '%s'" % [ @resource[:name].sub("@", "'@'"), @resource.should(:password_hash) ]
-		mysql_flush
-	end
-
-	def destroy
-		mysql defaults_file, "mysql", "-e", "drop user '%s'" % @resource[:name].sub("@", "'@'")
-		mysql_flush
-	end
-
-	def exists?
-		not mysql(defaults_file, "mysql", "-NBe", "select '1' from user where CONCAT(user, '@', host) = '%s'" % @resource[:name]).empty?
-	end
-
-	def password_hash
-		@property_hash[:password_hash]
-	end
-
-	def password_hash=(string)
-		mysql defaults_file, "mysql", "-e", "SET PASSWORD FOR '%s' = '%s'" % [ @resource[:name].sub("@", "'@'"), string ]
-		mysql_flush
-	end
 end
-
